4d6f012d7b380381dc26bd46f369181b53c9850c
navi107 <naveensing575@gmail.com>
2021-03-11 20:02:29 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ function App() {
   return (
     <Switch>
 
-      <Route path="/">
+      <Route exact path="/">
           This is home page
+      </Route>
+      <Route exact path="/starred">
+          This is starred page
+      </Route>
+      <Route>
+          Page not found!
       </Route>
     </Switch>
   );
